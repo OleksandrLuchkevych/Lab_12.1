b23d136f7dea79d4d14984d39b13212f97773507
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2023-03-31 00:50:48 +0300

    Add project files.

---
+++ Lab_12.1/Lab_12.1.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	struct Elem
+	{
+		Elem *link1,
+			 *link2;
+		int info;
+	};
+
+	Elem* p;
+
+	p = new Elem;
+	p->info = 1;
+	p->link1 = NULL;
+	p->link2 = new Elem;
+	p->link2->info = 2;
+	p->link2->link1 = new Elem;
+	p->link2->link1->info = 3;
+	p->link2->link1->link1 = p->link2;
+	p->link2->link2 = new Elem;
+	p->link2->link2->info = 4;
+	p->link2->link2->link1 = p;
+	p->link2->link2->link2 = p;
+	p->link2->link1->link2 = p->link2->link2;
+
+	delete p->link2->link2;
+	delete p->link2->link1;
+	delete p->link2->link1;
+	delete p;
+	
+
+}
